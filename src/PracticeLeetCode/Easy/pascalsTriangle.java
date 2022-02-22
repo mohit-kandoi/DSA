35b494a7b8bab5a164eb416328fa1219a719e884
mohit-kandoi <mohitkandoi65@gmail.com>
2022-02-22 20:46:20 +0530

    Day 48

---
+++ src/PracticeLeetCode/Easy/pascalsTriangle.java
@@ -0,0 +1,11 @@
+package PracticeLeetCode.Easy;
+
+public class pascalsTriangle {
+    public static void main(String[] args) {
+
+        // for finding the particular element in pascal's triangle
+        // row-1
+        //      C
+        //       col-1
+    }
+}
